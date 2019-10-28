36a5a9140ec1d2af5d8b2870dcbecc0dbda94eac
Miguel Lopes <miguel.lopes@miniclip.com>
2019-10-28 12:57:47 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Breakout Game
+//
+//  Created by Miguel Lopes on 28/10/2019.
+//  Copyright © 2019 Miguel Lopes. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
