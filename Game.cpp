e17f3d1151ccadb1316fa3e06821a20959c8ae76
Miguel Lopes <miguel.lopes@miniclip.com>
2020-02-10 13:02:36 +0000

    fix game level

--- Game.cpp
+++ Game.cpp
@@ -35,8 +35,25 @@ void Game::Init(){
     ResourceManager::GetShader("sprite").Use().SetInteger("image", 0);
     ResourceManager::GetShader("sprite").SetMatrix4("projection", projection);
     // Load textures
+    // Load textures
+    ResourceManager::LoadTexture("Resources/background.jpg", GL_FALSE, "background");
     ResourceManager::LoadTexture("Resources/awesomeface.png", GL_TRUE, "face");
-    // Set render-specific controls
+    ResourceManager::LoadTexture("Resources/block.png", GL_FALSE, "block");
+    ResourceManager::LoadTexture("Resources/block_solid.png", GL_FALSE, "block_solid");
+    // Load levels
+    GameLevel one;
+    one.Load("Resources/levels/one.lvl", this->Width, this->Height * 0.5);
+    GameLevel two;
+    two.Load("Resources/levels/two.lvl", this->Width, this->Height * 0.5);
+    GameLevel three;
+    three.Load("Resources/levels/three.lvl", this->Width, this->Height * 0.5);
+    GameLevel four;
+    four.Load("Resources/levels/four.lvl", this->Width, this->Height * 0.5);
+    this->Levels.push_back(one);
+    this->Levels.push_back(two);
+    this->Levels.push_back(three);
+    this->Levels.push_back(four);
+    this->Level = 1;    // Set render-specific controls
     Shader myShader;
     myShader = ResourceManager::GetShader("sprite");
     Renderer = new SpriteRenderer(myShader);
@@ -53,10 +70,15 @@ void Game::ProcessInput(GLfloat dt){
 
 void Game::Render(){
     Texture2D myTexture;
-    myTexture = ResourceManager::GetTexture("face");
-    Renderer->DrawSprite(myTexture,
-                         glm::vec2(200, 200),
-                         glm::vec2(300, 400),
-                         45.0f,
-                         glm::vec3(0.0f, 1.0f, 0.0f));
+    myTexture = ResourceManager::GetTexture("background");
+    if(State == GAME_ACTIVE){
+        // Draw background
+        Renderer->DrawSprite(myTexture,
+                             glm::vec2(0, 0),
+                             glm::vec2(this->Width,
+                                       this->Height),
+                             0.0f);
+        // Draw level
+        this->Levels[this->Level].Draw(*Renderer);
+    }
 }
